e0e47b438a57a907214a2bdfdeec9dfb3465de8f
shiteyaru <andreyuriel4@gmail.com>
2024-12-18 13:53:21 -0300

    Melhorando a implementacao do metodo ImprimirTabuleiro

--- Tela.cs
+++ Tela.cs
@@ -7,19 +7,36 @@ namespace xadrex_console {
         public static void ImprimirTabuleiro(Tabuleiro tab) {
 
             for (int i = 0; i < tab.Linhas; i++) {
+                Console.Write($"{8 - i} ");
                 for (int j = 0; j < tab.Colunas; j++) {
 
                     if (tab.peca(i, j) == null) {
                         Console.Write($"- ");
                     }
                     else {
-                        Console.Write($"{tab.peca(i, j)} ");
+                        Tela.ImprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
-
+                    
                 }
                 Console.WriteLine();
 
 
+            }
+            Console.WriteLine("  a b c d e f g h");
+
+        }
+
+        public static void ImprimirPeca(Peca peca) {
+
+            if (peca.Cor == Cor.Branca) {
+                Console.Write($"{peca}");
+            }
+            else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
 
         }
