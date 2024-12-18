a649e6ec2fe153d6ab8a6e7ee037bdbe27969b59
shiteyaru <andreyuriel4@gmail.com>
2024-12-18 11:09:57 -0300

    Tratando erros com exceção

--- Program.cs
+++ Program.cs
@@ -7,13 +7,22 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try {
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tela.ImprimirTabuleiro(tab);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
+
+                Tela.ImprimirTabuleiro(tab);
+                
+            } 
+            catch (TabuleiroException e) {
+                
+                Console.WriteLine(e.Message);
+
+            }
 
             Console.ReadLine();
 
